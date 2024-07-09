9dcbe76b8813e363dc4747f3d0c290c4165b6ed4
sayamiky <irmakrisnayanti31@gmail.com>
2024-07-09 09:13:55 +0800

    .

--- vue-js-project/tasty-recipe/src/store/recipe.js
+++ vue-js-project/tasty-recipe/src/store/recipe.js
@@ -1,33 +1,35 @@
 import axios from "axios";
 
 export default {
-    namespaced : true,
-    state(){
-        return {
-            recipes: []
-        }
+  namespaced: true,
+  state() {
+    return {
+      recipes: [],
+    };
+  },
+  getters: {},
+  mutations: {
+    setRecipeData(state, payload) {
+      state.recipes = payload;
     },
-    getters: {},
-    mutations: {
-        setRecipeData(state, payload){
-            state.recipes = payload
+  },
+  actions: {
+    async getRecipeData({ commit }) {
+      try {
+        // tda : https://recipe-vue-batch2-defaut-rtdb.firebaseio.com/recipes.json
+        // ms irma : https://recipe-vue-5fb49-default-rtdb.firebaseio.com/recipes.json
+        //mr wen : https://vue-js-project-5e9a5-default-rtdb.firebaseio.com/recipes.json
+        const { data } = await axios.get(
+          "https://recipe-vue-5fb49-default-rtdb.firebaseio.com/recipes.json"
+        );
+        const arr = [];
+        for (let key in data) {
+          arr.push({ id: key, ...data[key] });
         }
+        commit("setRecipeData", arr);
+      } catch (err) {
+        console.log(err);
+      }
     },
-    actions: {
-        async getRecipeData({commit}){
-            try {
-                // tda : https://recipe-vue-batch2-defaut-rtdb.firebaseio.com/recipes.json
-                // ms irma : https://recipe-vue-5fb49-default-rtdb.firebaseio.com/recipes.json
-                //mr wen : https://vue-js-project-5e9a5-default-rtdb.firebaseio.com/recipes.json
-                const {data} = await axios.get("https://recipe-vue-5fb49-default-rtdb.firebaseio.com/recipes.json")
-                const arr = []
-                for (let key in data) {
-                    arr.push({id: key,...data[key]});
-                }
-                commit("setRecipeData", arr)
-            } catch (err) {
-                console.log(err)
-            }
-        }
-    }
-}
+  },
+};
